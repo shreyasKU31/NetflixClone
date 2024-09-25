0b822366168e8b7b20e424e1b3e8054d6570de36
Shreyas_K_U <shreyasku31@gmail.com>
2024-09-25 15:41:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let qna = document.querySelector('.qna');
+let ans = document.querySelector('.ans');
+
+qna.addEventListener('click', () => {
+    if(ans.style.display == 'none'){
+        ans.style.display = 'block';
+    }
+});
+
+
